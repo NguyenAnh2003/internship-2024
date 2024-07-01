b6590c73f16297b7cb7ee61e6a162b4088b5350a
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-07-01 13:44:02 +0700

    #code

--- data_manipulation/data_processing.py
+++ data_manipulation/data_processing.py
@@ -1,6 +1,28 @@
-import torch
+from omegaconf import OmegaConf, DictConfig
+import re
+
 
 class DataProcessPipeline:
     # data processing pipeline with aspect based sentiment analysis
-    def __init__(self) -> None:
-        pass
+    def __init__(self, conf: DictConfig = None) -> None:
+        # input as yaml file
+        self.conf = OmegaConf.create(conf)
+
+    def processing_step(self, path=None, out_path=None):
+        # json file
+        json_file = open(path, "r", encoding="utf-8")
+
+        pattern = r"\n([0-9][0-9][0-9])\."
+        
+        str0 = re.sub(
+            pattern,
+            "",
+            "\n100. The train was a bit slow, but it was still a comfortable way to get around the city.",
+        )
+
+        # out_file = open(out_path, 'w', encoding='utf-8')
+
+
+if __name__ == "__main__":
+    pipeline = DataProcessPipeline()
+    pipeline.processing_step("./data_manipulation/metadata/gen_ds.json")
