595868c65b56a469e805b8de5dde450bbaf8f6f1
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-07-04 08:54:20 +0700

    #code

--- data_manipulation/data_generator.py
+++ data_manipulation/data_generator.py
@@ -2,47 +2,42 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 import json
+from omegaconf import DictConfig, OmegaConf
 
 load_dotenv()
 
-genai.configure(api_key=os.environ["GEMINI_APIKEY"])
-
-model = genai.GenerativeModel("gemini-1.5-flash")
 
 class Generator:
-    def __init__(self) -> None:
-        pass
-    
-    def prompt_template(self):
-        pass
-    
-    def generate_ds(self):
-        pass
+    def __init__(self, conf: DictConfig = None) -> None:
+        # config
+        self.conf = OmegaConf.create(conf)
+
+        # api key for llm
+        api_key = api_key = os.environ["GEMINI_APIKEY"]
+        if self.conf.model.llm.gemini == True:
+            # config gemini
+            # call with model name
+            genai.configure(api_key=api_key)
+            self.llm = genai.GenerativeModel(self.conf.model.llm.name)
+
+    def generate_ds(self, out_path: str) -> None:
+        # out file format is json
+        jsonfile = open(out_path, "a", encoding="utf-8")
+
+        prompt_template = "Give me 100 humanity feedbacks of tourists about Bangkok train service, include tourist's feelings, ending of each example is > sign, the dataset must be natural don't start the sentence identically"
+        response = self.llm.generate_content(prompt_template)
+
+        # print(response.text)
+
+        ds = response.text
+        ds = ds.split(">")
+
+        for i, item in enumerate(ds):
+            data_package = {f"feeback {i}": item}
+            json.dump(data_package, jsonfile, ensure_ascii=False)
+            jsonfile.write("\n")
 
 
-def prompt_template(example: str) -> str:
-    
-    return
-
-
-def gen_ds():
-    jsonfile = open("./data_manipulation/metadata/gen_ds.json", "a", encoding="utf-8")
-
-    prompt_template = "Give me 100 humanity feedbacks of tourists about Bangkok train service, include tourist's feelings, ending of each example is > sign, the dataset must be natural don't start the sentence identically"
-    response = model.generate_content(prompt_template)
-
-    # print(response.text)
-
-    ds = response.text
-    ds = ds.split(">")
-
-    for i, item in enumerate(ds):
-        data_package = {f"feeback {i}": item}
-        json.dump(data_package, jsonfile, ensure_ascii=False)
-        jsonfile.write("\n")
-
-
-for _ in range(100):
-    gen_ds()
-
-print("DONE")
+if __name__ == "__main__":
+    generator = Generator()
+    print("DONE")
--- data_manipulation/data_processing.py
+++ data_manipulation/data_processing.py
@@ -16,40 +16,7 @@ class DataProcessPipeline:
         # self.conf.model.llm.name
         # )  # name of llm to preprocess data
 
-    def processing_step(self, path: str = None, out_path: str = None):
-        result = []
-        # json file
-        json_file = open(path, "r", encoding="utf-8")
-        dataset = json.load(json_file)
-
-        # processing each data point
-        for idx, point in enumerate(dataset):
-            self.remove_emoji(point["reviewText"])  # remove emoji in text
-            self.remove_emoji(point["ratingText"])  # remove emoji in rating text
-
-            # remove html tags
-            point["reviewText"] = point["reviewText"].replace("<br />", " ")
-
-            # remove punctuation
-            point["reviewText"] = self.remove_punctuation(point["reviewText"])
-            # point["reviewTitle"] = self.remove_punctuation(point["reviewTitle"])
-
-            # detect language and group ds
-            lang = detect(point["reviewText"])
-            if lang == "en":
-                result.append(point)
-
-        out_file = open(out_path, "w", encoding="utf-8")
-        json.dump(result, out_file, indent=4, ensure_ascii=False)
-
-    def remove_punctuation(self, string):
-        # Replace specific punctuation characters
-        for char in [",", ".", "!", "?", ";", ":"]:
-            string = string.replace(char, "")
-        return string
-
-    def remove_emoji(self, string):
-        emoji_pattern = re.compile(
+        self.emoji_pattern = re.compile(
             "["
             "\U0001F600-\U0001F64F"  # emoticons
             "\U0001F300-\U0001F5FF"  # symbols & pictographs
@@ -74,7 +41,41 @@ class DataProcessPipeline:
             flags=re.UNICODE,
         )
 
-        emoji_pattern.sub(r"", string)
+    def processing_step(self, path: str = None, out_path: str = None):
+        result = []
+        # json file
+        json_file = open(path, "r", encoding="utf-8")
+        dataset = json.load(json_file)
+
+        # processing each data point
+        for idx, point in enumerate(dataset):
+            self.remove_emoji(point["reviewText"])  # remove emoji in text
+            self.remove_emoji(point["ratingText"])  # remove emoji in rating text
+
+            # remove html tags
+            point["reviewText"] = point["reviewText"].replace("<br />", " ")
+
+            # remove punctuation
+            point["reviewText"] = self.remove_punctuation(point["reviewText"])
+            # point["reviewTitle"] = self.remove_punctuation(point["reviewTitle"])
+
+            # detect language and group ds
+            lang = detect(point["reviewText"])
+            if lang == "en":
+                result.append(point)
+
+        # write data to out file
+        out_file = open(out_path, "w", encoding="utf-8")
+        json.dump(result, out_file, indent=4, ensure_ascii=False)
+
+    def remove_punctuation(self, string):
+        # Replace specific punctuation characters
+        for char in [",", ".", "!", "?", ";", ":", "<", ">", "/", "-"]:
+            string = string.replace(char, "")
+        return string
+
+    def remove_emoji(self, string):
+        self.emoji_pattern.sub(r"", string)
 
     def prepreprocesse_ds(self, path: str, out_path: str = None):
         json_file = open(path, "r", encoding="utf-8")
@@ -120,5 +121,5 @@ if __name__ == "__main__":
     pipeline = DataProcessPipeline()
     path = "./data_manipulation/metadata/train.json"
     opath = "./data_manipulation/metadata/processed_train.json"
-    pipeline.processing_step(path=path, out_path=opath)
+    pipeline.processing_step(path=opath, out_path=opath)
     print("DONE")
