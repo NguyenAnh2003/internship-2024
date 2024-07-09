e20b9a2effe14e7cc851b097185f56cb6c119788
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-07-09 15:55:57 +0700

    #code

--- data_manipulation/data_generator.py
+++ data_manipulation/data_generator.py
@@ -43,25 +43,6 @@ class Generator:
         response = self.llm.generate_content(prompt_template)
         return response.text
 
-    def generate_ds(self, out_path: str = None) -> None:
-        # out file format is json
-        # jsonfile = open(out_path, "a", encoding="utf-8")
-
-        prompt_template = "Give me 100 humanity feedbacks of tourists about Bangkok train service, include tourist's feelings, ending of each example is > sign, the dataset must be natural don't start the sentence identically"
-        response = self.llm.generate_content(prompt_template)
-
-        # print(response.text)
-
-        ds = response.text
-        # ds = ds.split(">")
-
-        print(ds)
-
-        # for i, item in enumerate(ds):
-        #     data_package = {f"feeback {i}": item}
-        #     json.dump(data_package, jsonfile, ensure_ascii=False)
-        #     jsonfile.write("\n")
-
 
 if __name__ == "__main__":
 
