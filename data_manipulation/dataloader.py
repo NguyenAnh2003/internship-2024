5a8a2328f7913749dc11092c2f5cd019e0d6173d
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-07-10 15:04:48 +0700

    #code

--- data_manipulation/dataloader.py
+++ data_manipulation/dataloader.py
@@ -2,26 +2,25 @@ from torch.utils.data import Dataset, DataLoader
 import pandas as pd
 from datasets import Dataset as HFDataset
 from omegaconf import OmegaConf, DictConfig
+from libs.helper_functions import get_configs
 
 
 class ABSADataset(Dataset):
-    def __init__(self, tokenizer, csv_path, conf: DictConfig = None) -> None:
+    def __init__(self, tokenizer = None, csv_path = "", conf: DictConfig = None) -> None:
         super().__init__()
         self.conf = OmegaConf.create(conf)
         self.tokenizer = tokenizer  # tokenizer as transform
         self.dataset = self._create_hf_ds(csv_path=csv_path)  # create HF dataset
 
     def __getitem__(self, index):
-        prompt, review, aspect, opinion, polarity = self._instruction_template4train(
-            idx=index
-        )
-        return {prompt, review, aspect, opinion, polarity}
+        pass
 
     def _instruction_template4train(self, index):
         review = self.dataset[index]["review"]  # tokenize this
-        aspect = self.dataset[index]["aspect"]
-        opinion = self.dataset[index]["opinion"]
-        polarity = self.dataset[index]["polarity"]
+        aspect = self.dataset[index]["aspect"] # label2index
+        opinion = self.dataset[index]["opinion"] # tokenize this
+        polarity = self.dataset[index]["polarity"] # label2index
+        prompt = ""
         # take review as input and aspect, polarity as output
         if self.conf.model.instruction_style == "simple":
             prompt = f""" 
@@ -46,3 +45,9 @@ class ABSADataset(Dataset):
 class ABSAloader(DataLoader):
     def __init__(self) -> None:
         super.__init__()
+
+if __name__ == "__main__":
+    conf = get_configs("../configs/instruction_tuning_absa.yaml")
+    ds = ABSADataset(conf,
+                     csv_path="../data_manipulation/metadata/generated-manifest.csv")
+    print(ds.dataset)
