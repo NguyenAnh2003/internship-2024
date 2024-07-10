c3e2a2bcb0aa96e5579c349d6f069e595a358d34
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-07-10 15:36:37 +0700

    #code

--- data_manipulation/dataloader.py
+++ data_manipulation/dataloader.py
@@ -6,31 +6,31 @@ from libs.helper_functions import get_configs
 
 
 class ABSADataset(Dataset):
-    def __init__(self, tokenizer = None, csv_path = "", conf: DictConfig = None) -> None:
+    def __init__(self, tokenizer=None, csv_path="", conf: DictConfig = None) -> None:
         super().__init__()
         self.conf = OmegaConf.create(conf)
         self.tokenizer = tokenizer  # tokenizer as transform
         self.dataset = self._create_hf_ds(csv_path=csv_path)  # create HF dataset
+        self.dataset = self.dataset.map(
+            self._instruction_template4train
+        )  # mapping data
 
     def __getitem__(self, index):
         pass
 
-    def _instruction_template4train(self, index):
-        review = self.dataset[index]["review"]  # tokenize this
-        aspect = self.dataset[index]["aspect"] # label2index
-        opinion = self.dataset[index]["opinion"] # tokenize this
-        polarity = self.dataset[index]["polarity"] # label2index
+    def _instruction_template4train(self, batch):
+        # prepare HF dataset
+        review = batch["review"]
+        aspect = batch["aspect"]  # label2index
+        opinion = batch["opinion"]  # tokenize this
+        polarity = batch["polarity"]  # label2index
         prompt = ""
         # take review as input and aspect, polarity as output
         if self.conf.model.instruction_style == "simple":
-            prompt = f""" 
-            ###Instruct: Follow the given review and the corresponded output
-            try to brainstorm and predict the aspect and polarity of that aspect in the review
-            ###Input: {review}
-            ###Output: aspect: {aspect} polarity: {polarity}
-            """
+            prompt = f"""###Instruct: Follow the given review and the corresponded output try to brainstorm and predict the aspect and polarity of that aspect in the review ###Input: {review} ###Output: aspect: {aspect} polarity: {polarity} opinion: {opinion} """
+        batch["prompt"] = prompt
 
-        return prompt, review, aspect, opinion, polarity
+        return batch
 
     @staticmethod
     def _create_hf_ds(csv_path: str):
@@ -38,16 +38,24 @@ class ABSADataset(Dataset):
         ds = HFDataset.from_pandas(train_csv)
         return ds
 
+    def show_metadata(self):
+        for i, row in enumerate(self.dataset):
+            review = row["review"]
+            aspect = row["aspect"]
+            opinion = row["opinion"]
+            polarity = row["polarity"]
+            prompt = row["prompt"]
+            result = {review, prompt, aspect, opinion, polarity}
+            print(f"Point: {i} dict: {result}")
+
     def __len__(self):
         return len(self.dataset)
 
 
-class ABSAloader(DataLoader):
-    def __init__(self) -> None:
-        super.__init__()
-
 if __name__ == "__main__":
     conf = get_configs("../configs/instruction_tuning_absa.yaml")
-    ds = ABSADataset(conf,
-                     csv_path="../data_manipulation/metadata/generated-manifest.csv")
-    print(ds.dataset)
+    # conf["model"]["pretrained"]["name"] = "google-bert/bert-base-multilingual-cased"
+
+    ds = ABSADataset(conf=conf, csv_path="metadata/generated-manifest.csv")
+
+    ds.show_metadata()
