41aa9cd1738b739177a3dde772c2656b6a491484
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-08-04 22:55:17 +0700

    #code

--- build/lightning_trainer.py
+++ build/lightning_trainer.py
@@ -1,13 +1,12 @@
 from typing import Any
-
 from modules.absa_light_module import ABSALightningModule
 from libs.helper_functions import get_configs
 from build.model import ABSAModel
 from pytorch_lightning import Trainer
-from torch.nn import CrossEntropyLoss
+from pytorch_lightning.loggers import WandbLogger
 
 if __name__ == "__main__":
-
+  wandb_logger = WandbLogger(project="absa_", log_model="all")
   # init Trainer
   trainer = Trainer(default_root_dir="../checkpoints")
 
@@ -20,23 +19,14 @@ if __name__ == "__main__":
   conf["model"]["train"]["test_dir"] = "../data_manipulation/metadata/manifests/test.csv"
 
   model = ABSAModel(conf)
+  wandb_logger.watch(model) # watch model grad
   tokenizer = model.tokenizer
   module_absa = ABSALightningModule(tokenizer=tokenizer, conf=conf, model=model)
 
   # TRAIN, DEV SET
   train_ds, dev_ds = module_absa.setup_train_dataloader()
-
-  loss_fn = CrossEntropyLoss()
-  sample = next(iter(train_ds))
-  input_ids = sample["input_ids"]
-  attention_mask = sample["attention_mask"]
-  labels = sample["labels"]
-  logits = model(input_ids=input_ids, attention_mask=attention_mask)
-  loss = loss_fn(logits, labels)
-  print(loss)
-
   # TEST SET
-  # test_ds = module_absa.setup_test_dataloader()
-  # trainer.fit(module_absa, train_ds, dev_ds) # train
+  test_ds = module_absa.setup_test_dataloader()
+  trainer.fit(module_absa, train_ds, dev_ds) # train
 
 
