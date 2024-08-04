7e42d2c050ad84c7a4d838dfb021653291a8b35b
NguyenAnh2003 <cunho2803032003@gmail.com>
2024-08-04 22:56:49 +0700

    #code

--- build/lightning_trainer.py
+++ build/lightning_trainer.py
@@ -6,27 +6,31 @@ from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
 
 if __name__ == "__main__":
-  wandb_logger = WandbLogger(project="absa_", log_model="all")
-  # init Trainer
-  trainer = Trainer(default_root_dir="../checkpoints")
+    wandb_logger = WandbLogger(project="absa_", log_model="all")
+    # init Trainer
+    trainer = Trainer(default_root_dir="../checkpoints")
 
-  conf = get_configs("../configs/absa_model.yaml")
+    conf = get_configs("../configs/absa_model.yaml")
 
-  conf["model"]["pretrained"]["name"] = "FacebookAI/xlm-roberta-base"
-  conf["model"]["pretrained"]["freeze"] = True
-  conf["model"]["train"]["train_dir"] = "../data_manipulation/metadata/manifests/train.csv"
-  conf["model"]["train"]["dev_dir"] = "../data_manipulation/metadata/manifests/val.csv"
-  conf["model"]["train"]["test_dir"] = "../data_manipulation/metadata/manifests/test.csv"
+    conf["model"]["pretrained"]["name"] = "FacebookAI/xlm-roberta-base"
+    conf["model"]["pretrained"]["freeze"] = True
+    conf["model"]["train"][
+        "train_dir"
+    ] = "../data_manipulation/metadata/manifests/train.csv"
+    conf["model"]["train"][
+        "dev_dir"
+    ] = "../data_manipulation/metadata/manifests/val.csv"
+    conf["model"]["train"][
+        "test_dir"
+    ] = "../data_manipulation/metadata/manifests/test.csv"
 
-  model = ABSAModel(conf)
-  wandb_logger.watch(model) # watch model grad
-  tokenizer = model.tokenizer
-  module_absa = ABSALightningModule(tokenizer=tokenizer, conf=conf, model=model)
+    model = ABSAModel(conf)
+    wandb_logger.watch(model)  # watch model grad
+    tokenizer = model.tokenizer
+    module_absa = ABSALightningModule(tokenizer=tokenizer, conf=conf, model=model)
 
-  # TRAIN, DEV SET
-  train_ds, dev_ds = module_absa.setup_train_dataloader()
-  # TEST SET
-  test_ds = module_absa.setup_test_dataloader()
-  trainer.fit(module_absa, train_ds, dev_ds) # train
-
-
+    # TRAIN, DEV SET
+    train_ds, dev_ds = module_absa.setup_train_dataloader()
+    # TEST SET
+    test_ds = module_absa.setup_test_dataloader()
+    trainer.fit(module_absa, train_ds, dev_ds)  # train
